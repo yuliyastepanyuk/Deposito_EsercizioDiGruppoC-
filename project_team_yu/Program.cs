b96499857520f0bd945831e51f3ecdc36f8c2170
yuliya <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-05-21 17:28:32 +0200

    Add files via upload

---
+++ project_team_yu/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+public class CorsoMusica : Corso
+{
+    protected string Strumento;
+
+    public CorsoMusica(string nomeCorso, int durataOre, string docente, List<string> studenti, string strumento) : base(nomeCorso, durataOre, docente, studenti)
+    {
+        Strumento = strumento;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" Il corso è di {Strumento}.";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si tiene una prova pratica dello strumento: {Strumento}.");
+    }
+
+    public static CorsoMusica AggiungiCorsoMusica(string nomeCorso, int durataOre, string docente, List<string> studenti, string strumento)
+    {
+        Console.WriteLine("Aggiungi un nuovo corso di musica.");
+        string NomeCorso = Console.ReadLine();
+        Console.WriteLine("Aggiungi la durata del corso in ore.");
+        int DurataOre = int.Parse(Console.ReadLine());
+        Console.WriteLine("Aggiungi il nome del docente.");
+        string Docente = Console.ReadLine();
+        Console.WriteLine("Aggiungi il nome dello strumento.");
+        string Strumento = Console.ReadLine();
+        CorsoMusica corsoMusica = new CorsoMusica(NomeCorso, DurataOre, Docente, studenti, Strumento);
+        return corsoMusica;
+    }
+
+} //Questo non è corretto: List<string> studenti è la dichiarazione del tipo, non il nome della variabile. Quando chiami il costruttore base, devi passare la variabile che contiene la lista, cioè solo studenti.
+
+public class CorsoPittura : Corso
+{
+    protected string Tecnica;
+
+    public CorsoPittura(string nomeCorso, int durataOre, string docente, List<string> studenti, string tecnica) : base(nomeCorso, durataOre, docente, studenti)
+    {
+        Tecnica = tecnica;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" Il corso è di {Tecnica}.";
+    }
+
+    public override void MetodoSpeciale()
+    {
+        Console.WriteLine($"Si lavora su una tela con tecnica: {Tecnica}");
+    }
+
+    public static CorsoPittura AggiungiCorsoPittura(string nomeCorso, int durataOre, string docente, List<string> studenti, string tecnica)
+    {
+        Console.WriteLine("Aggiungi un nuovo corso di pittura.");
+        string NomeCorso = Console.ReadLine();
+        Console.WriteLine("Aggiungi la durata del corso in ore.");
+        int DurataOre = int.Parse(Console.ReadLine());
+        Console.WriteLine("Aggiungi il nome del docente.");
+        string Docente = Console.ReadLine();
+        Console.WriteLine("Aggiungi la tecnica di pittura.");
+        string Tecnica = Console.ReadLine();
+        CorsoPittura corsoPittura = new CorsoPittura(NomeCorso, DurataOre, Docente, studenti, Tecnica);
+        return corsoPittura;
+    }
+}
---
+++ project_team_yu/Program_classebase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Corso
+{
+    protected string NomeCorso;
+    protected int DurataOre;
+    protected string Docente;
+    protected List<string> Studenti = new List<string>();
+
+    public Corso(string nomeCorso, int durataOre, string docente, List<string> studenti) // costruttore
+    {
+        NomeCorso = nomeCorso;
+        DurataOre = durataOre;
+        Docente = docente;
+        Studenti = studenti;
+    }
+
+    public void AggiungiStudente(string nomeStudente)
+    {
+        Studenti.Add(nomeStudente);
+    }
+
+    public virtual string ToString()
+    {
+        return $"Il nome del corso è {NomeCorso}, la durata è {DurataOre} ore, il docente è {Docente}.";
+    }
+
+    public virtual void MetodoSpeciale()
+    {
+        Console.WriteLine("Questo è un metodo speciale della classe Corso.");
+    }
+}
